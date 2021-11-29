68e0c2a206f15cb7c8e4cd85d322be4035385004
Danila Krylov <danila.krylov.1980@mail.ru>
2021-11-29 18:28:35 +0300

    first commit

---
+++ iOSArchitecturesDemo/Flows/AppDetail/Childs/CurrentVersionDetails/VersionController/VersionController.swift
@@ -0,0 +1,9 @@
+//
+//  VersionController.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Крылов Данила  on 27.11.2021.
+//  Copyright © 2021 ekireev. All rights reserved.
+//
+
+import Foundation
---
+++ iOSArchitecturesDemo/Flows/AppDetail/Childs/CurrentVersionDetails/VersionView/VersionView.swift
@@ -0,0 +1,9 @@
+//
+//  VersionView.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Крылов Данила  on 27.11.2021.
+//  Copyright © 2021 ekireev. All rights reserved.
+//
+
+import Foundation
